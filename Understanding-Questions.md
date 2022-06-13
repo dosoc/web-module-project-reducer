7a576e59ea03481eba011cc798ac746ce4673246
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-06-13 14:45:53 -0500

    adds funtionality to number clicks and operator changes

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,10 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
+* the handleAddOne function is called.
+* the dispatch is invoked with (addone())
+* addone() returns {type:ADD_ONE} as the argument to dispatch()
+* dispatch passes {type:ADD_ONE} to the reducer fuction which returns a new state with an updated state total +1
+* the new state total is set to state and the new data renders on to the screen
 
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+import reducer from '../reducers/index'
+import {initialState} from '../reducers/index'
+
+import { applyNumber, changeOperation } from '../actions/index'
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // const handleAddOne = () => {
+  //   dispatch(addOne())
+  // }
+
+  const handleApplyNumber = (number) => {
+    dispatch(applyNumber(number))
+  }
+
+  const handleChangeOperator = (operator) => {
+    dispatch(changeOperation(operator))
+  }
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,10 +34,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -29,27 +47,27 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={()=>handleApplyNumber(1)} value={1}/>
+              <CalcButton onClick={()=>handleApplyNumber(2)} value={2}/>
+              <CalcButton onClick={()=>handleApplyNumber(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={()=>handleApplyNumber(4)} value={4}/>
+              <CalcButton onClick={()=>handleApplyNumber(5)} value={5}/>
+              <CalcButton onClick={()=>handleApplyNumber(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={()=>handleApplyNumber(7)} value={7}/>
+              <CalcButton onClick={()=>handleApplyNumber(8)} value={8}/>
+              <CalcButton onClick={()=>handleApplyNumber(9)} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={()=>handleChangeOperator("+")} value={"+"}/>
+              <CalcButton onClick={()=>handleChangeOperator("*")} value={"*"}/>
+              <CalcButton onClick={()=>handleChangeOperator("-")} value={"-"}/>
             </div>
 
             <div className="row ce_button">
